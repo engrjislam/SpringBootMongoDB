10249237d1c9d2f1fcd2c5adf3f9ee45a89bf7b3
engr.jislam <engr.jislam@gmail.com>
2017-03-02 20:00:02 +0200

    First-commit

---
+++ src/main/java/com/maxpro/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.maxpro.repository;
+
+import com.maxpro.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+    User findByUsername(String userName);
+
+}
